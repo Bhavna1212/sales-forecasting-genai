f1aa32e699e9aa6f871175557bc36aa7c8c44ffa
Bhavna Chauhan <72153672+Bhavna1212@users.noreply.github.com>
2025-02-20 15:50:46 -0500

    Update sales.py

--- sales.py
+++ sales.py
@@ -132,5 +132,5 @@ def predict():
 import os
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT"))  # Use Render's PORT or default to 5000
+    port = int(os.getenv("PORT", 5000))  # Use Render's PORT or default to 5000
     app.run(host="0.0.0.0", port=port)
